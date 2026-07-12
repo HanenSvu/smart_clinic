e9519d695c3dee1af290f8325d84f4b6d58ca9ee
unknown <hanenfash1999@gmail.com>
2026-07-12 12:33:28 +0300

    Fix: Enable cleartext traffic for HTTP connections

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
   
-  static const String baseUrl = 'http://192.168.1.106:8000/api';
+  static const String baseUrl = 'http://192.168.1.27:8000/api';
   
   
   static const String loginEndpoint = '/login';
